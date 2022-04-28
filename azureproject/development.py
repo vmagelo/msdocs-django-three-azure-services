d98c17513685ff53b341fd3fe4b835611be65c2d
vmagelo <51368918+vmagelo@users.noreply.github.com>
2022-04-28 21:57:10 +0200

    Move secret key to settings

--- azureproject/development.py
+++ azureproject/development.py
@@ -15,14 +15,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from .settings import *
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7ppocbnx@w71dcuinn*t^_mzal(t@o01v3fee27g%rg18fc5d@'
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
