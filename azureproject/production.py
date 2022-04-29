c1ffe8822ed2d32b520572bf71342d2f7bfe78be
vmagelo <51368918+vmagelo@users.noreply.github.com>
2022-04-29 17:40:16 +0200

    Debug.

--- azureproject/production.py
+++ azureproject/production.py
@@ -4,7 +4,9 @@ from .get_token import get_token
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+# ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = ['*']
+
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 DEBUG = False
 
