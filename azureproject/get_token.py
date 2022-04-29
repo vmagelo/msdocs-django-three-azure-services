f275bfaa0a438f6c9379635fe5bf0d3a3e09c084
vmagelo <51368918+vmagelo@users.noreply.github.com>
2022-04-29 11:12:21 +0200

    Remove print statements.

--- azureproject/get_token.py
+++ azureproject/get_token.py
@@ -13,11 +13,9 @@ def get_token():
         # Get token for Azure Database for PostgreSQL
         token = azure_credential.get_token("https://ossrdbms-aad.database.windows.net")
         DATABASES['default']['PASSWORD'] = token.token
-        print(DATABASES['default'])
     else:
         # Locally, read password from environment variable.
         from azureproject.development import DATABASES
         DATABASES['default']['PASSWORD'] = os.environ['DBPASS']
         print("Read password env variable.")
-        print(DATABASES['default'])
     return
