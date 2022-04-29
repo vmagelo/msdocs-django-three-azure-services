23aa75359cf7f554753363282694b0f7dd789745
vmagelo <51368918+vmagelo@users.noreply.github.com>
2022-04-29 17:59:45 +0200

    Access config not using import.

--- azureproject/get_token.py
+++ azureproject/get_token.py
@@ -1,5 +1,6 @@
 import os
 from azure.identity import DefaultAzureCredential
+import django.conf as conf
 
 def get_token():
     print("DJANGO_SETTINGS_MODULE = " + os.environ['DJANGO_SETTINGS_MODULE'])
@@ -7,16 +8,14 @@ def get_token():
         print("IDENTITY_ENDPOINT = " + os.environ['IDENTITY_ENDPOINT'])
     if 'WEBSITE_HOSTNAME' in os.environ:        
         print("WEBSITE_HOSTNAME = " + os.environ['WEBSITE_HOSTNAME'])
-        from azureproject.production import DATABASES
         # Azure hosted, refresh token that becomes password.
         azure_credential = DefaultAzureCredential()
         # Get token for Azure Database for PostgreSQL
         token = azure_credential.get_token("https://ossrdbms-aad.database.windows.net")
-        DATABASES['default']['PASSWORD'] = token.token
+        conf.settings.DATABASES['default']['PASSWORD'] = token.token
         print(DATABASES['default'])
     else:
         # Locally, read password from environment variable.
-        from azureproject.development import DATABASES
-        DATABASES['default']['PASSWORD'] = os.environ['DBPASS']
+        conf.settings.DATABASES['default']['PASSWORD'] = os.environ['DBPASS']
         print("Read password env variable.")
     return
