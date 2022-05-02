d69b3eeaca779c18a0a8aea98005a6939171578f
vmagelo <51368918+vmagelo@users.noreply.github.com>
2022-05-02 14:47:05 +0200

    Work on storage values

--- azureproject/get_token.py
+++ azureproject/get_token.py
@@ -3,17 +3,11 @@ from azure.identity import DefaultAzureCredential
 import django.conf as conf
 
 def get_token():
-    print("DJANGO_SETTINGS_MODULE = " + os.environ['DJANGO_SETTINGS_MODULE'])
-    if 'IDENTITY_ENDPOINT' in os.environ:    
-        print("IDENTITY_ENDPOINT = " + os.environ['IDENTITY_ENDPOINT'])
-    if 'WEBSITE_HOSTNAME' in os.environ:        
-        print("WEBSITE_HOSTNAME = " + os.environ['WEBSITE_HOSTNAME'])
         # Azure hosted, refresh token that becomes password.
         azure_credential = DefaultAzureCredential()
         # Get token for Azure Database for PostgreSQL
         token = azure_credential.get_token("https://ossrdbms-aad.database.windows.net")
         conf.settings.DATABASES['default']['PASSWORD'] = token.token
-        print(conf.settings.DATABASES['default'])
     else:
         # Locally, read password from environment variable.
         conf.settings.DATABASES['default']['PASSWORD'] = os.environ['DBPASS']
